7e8c81eff075e672ea6a5bb70247c377bb9db2bb
paulovitornovaes <72573443+paulovitornovaes@users.noreply.github.com>
2022-02-25 22:55:12 -0300

    Add project files.

---
+++ ByteBank.Portal/Controller/CambioController.cs
@@ -0,0 +1,39 @@
+﻿using ByteBank.Service.Cambio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Service.Controller
+{
+    public class CambioController
+    {
+        private ICambioService _cambioService;
+
+        public CambioController()
+        {
+            _cambioService = new CambioTesteService();
+        }
+
+        public string MXN()
+        {
+            var valorFinal = _cambioService.Calcular("MXN", "BRL", 30);
+            var nomeCompletoResource = "ByteBank.Portal.View.Cambio.MXN.html";
+            var assembly = Assembly.GetExecutingAssembly();
+            var streamRecurso = assembly.GetManifestResourceStream(nomeCompletoResource);
+
+            var streamLeitura = new StreamReader(streamRecurso);
+            var textoPagina = streamLeitura.ReadToEnd();
+            var textoResultado = textoPagina.Replace("VALOR_EM_REAIS", valorFinal.ToString());
+
+            return textoResultado;
+        }
+        public string USD()
+        {
+            return null;
+        }
+    }
+}
---
+++ ByteBank.Portal/Infraestrutura/WebApplication.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Infraestrutura
+{
+    public class WebApplication
+    {
+        private readonly string[] _prefixos;
+        public WebApplication(string[] prefixos)
+        {
+            _prefixos = prefixos ?? throw new ArgumentNullException(nameof(prefixos));
+        }
+        public void Iniciar()
+        {
+            while (true)
+            {
+                ManipularRequisicao();
+            }
+        }
+
+        private void ManipularRequisicao()
+        {
+            var httpListener = new HttpListener();
+
+            foreach (var prefixo in _prefixos)
+            {
+                httpListener.Prefixes.Add(prefixo);
+            }
+            httpListener.Start();
+
+            var contexto = httpListener.GetContext();
+
+            var requisicao = contexto.Request;
+            var resposta = contexto.Response;
+
+            var path = requisicao.Url.AbsolutePath;
+
+            var assembly = Assembly.GetExecutingAssembly();
+            
+            var nomeResource = Utilidades.ConverterPathParaNomeAssembly(path);
+
+            var resourceStream = assembly.GetManifestResourceStream(nomeResource);
+
+            if(resourceStream == null)
+            {
+                resposta.StatusCode = 404;
+                resposta.OutputStream.Close();
+            }
+            else
+            {
+                var bytesResource = new byte[resourceStream.Length];
+
+                resourceStream.Read(bytesResource, 0, (int)resourceStream.Length);
+
+                resposta.ContentType = Utilidades.ObterTipoDeConteudo(path);
+                resposta.StatusCode = 200;
+                resposta.ContentLength64 = resourceStream.Length;
+
+                resposta.OutputStream.Write(bytesResource, 0, bytesResource.Length);
+
+                resposta.OutputStream.Close();
+            }
+
+            httpListener.Stop();
+        }
+    }
+}
